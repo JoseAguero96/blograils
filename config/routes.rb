a57273ff7375c7d219904f8e91dad4d9113fdecd
JoseAguero96 <joseaguero.vg@gmail.com>
2017-11-01 17:17:54 -0300

    Inicial

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+
+  	get 'front/index'
+
+  	root 'front#index'
+
+  	resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
